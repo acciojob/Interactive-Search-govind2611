86ae21c0b94c160fef60277ea5e29ff4a3ecc9bc
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-06-11 09:11:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const searchxyz = document.querySelector('.search')
+const btnxyz = document.querySelector(".btn")
+const inputxyz = document.querySelector(".input")
+
+btnxyz.addEventListener("click",function(){
+    searchxyz.classList.toggle("active")
+    inputxyz.focus()
+})
